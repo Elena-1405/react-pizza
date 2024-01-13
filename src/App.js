c7bb81afb646d01785cdf819281c6e55a05fb18d
Elena Petrova <helenofspb@gmail.com>
2024-01-13 17:26:57 +0300

    5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import '../src/scss/app.scss';
 import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
+import { PizzaBlock } from './components/PizzaBlock';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         </div>    
         <h2 class="content__title">Все пиццы</h2>
         <div class="content__items">
-         
+          <PizzaBlock />
         </div>
       </div>
     </div>

4fde5d6830af0bcf828e3ab3d6f2f2fe1d545c8d
Elena Petrova <helenofspb@gmail.com>
2024-01-15 18:07:21 +0300

    6

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,35 @@
+import React from "react";
+import { useState } from "react";
+
+
 export function Categories() {
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  const OnClickCategory = (index) => {
+    setActiveCategory(index);
+  }
+
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианские',
+    'Гриль',
+    'Острые',
+    'Закрытые'
+  ]
+
     return (
       <div class="categories">
-              <ul>
-                <li class="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+        <ul>
+          {
+            categories.map((value, index) => 
+              <li
+                onClick={() => OnClickCategory(index)}
+                className={activeCategory === index ? 'active' : ''}>
+                {value}
+              </li>)
+          }   
+        </ul>
+      </div>
     )
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
+import pizzaLogo from '../assets/img/pizza-logo.svg'
+
+
 export function Header() {
     return (
       <div className="header">
           <div className="container">
             <div className="header__logo">
-              <img width="38" src='img/pizza-logo.svg' alt="Pizza logo" />
+              <img width="38" src={pizzaLogo} alt="Pizza logo" />
               <div>
                 <h1>React Pizza</h1>
-                <p>самая вкусная пицца во Вселенной</p>
+                <p>Cамая вкусная пицца во Вселенной</p>
               </div>
             </div>
             <div className="header__cart">

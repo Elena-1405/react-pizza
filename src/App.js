3e9818aef6eb558628749d81956f1f2ac542799e
Elena Petrova <helenofspb@gmail.com>
2024-01-17 20:51:36 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../src/scss/app.scss';
 import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
 import { PizzaBlock } from './components/PizzaBlock';
-import pizzas from './assets/pizza-db.json';
+//import pizzas from './assets/pizza-db.json';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://6562128cdcd355c0832487c7.mockapi.io/pizza-react')
+      .then((res) => res.json())
+      .then((arr) => 
+      setItems(arr));
+  }, []);
+    
+      
   return (
     <div className="wrapper">
     <Header />
@@ -19,7 +30,7 @@ function App() {
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
           {
-            pizzas.map(obj => 
+            items.map(obj => 
             <PizzaBlock
             key={obj.title}
             {...obj} />)

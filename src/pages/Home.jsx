b9f0ae651590be37f5eef17fd16d53c796556e5b
Elena Petrova <helenofspb@gmail.com>
2024-01-18 17:56:44 +0300

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+import { Categories } from '../components/Categories';
+import { Sort } from '../components/Sort';
+import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
+import { Skeleton } from '../components/PizzaBlock/Skeleton'
+//import pizzas from './assets/pizza-db.json';
+
+export const Home = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://6562128cdcd355c0832487c7.mockapi.io/pizza-react')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+    
+
+    return (
+        <>
+            <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>    
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+            : items.map(obj => 
+            <PizzaBlock
+            key={obj.id}
+            {...obj} />)
+          }
+        </div>
+        </>
+    )
+}

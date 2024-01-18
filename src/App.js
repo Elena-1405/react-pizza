97373c8ec96f99e819b581ea53ca716bb69baba9
Elena Petrova <helenofspb@gmail.com>
2024-01-18 17:24:08 +0300

    router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import '../src/scss/app.scss';
 import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
-import { PizzaBlock } from './components/PizzaBlock';
+import { PizzaBlock } from './components/PizzaBlock/PizzaBlock';
+import { Skeleton } from './components/PizzaBlock/Skeleton'
 //import pizzas from './assets/pizza-db.json';
 
 function App() {
-
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://6562128cdcd355c0832487c7.mockapi.io/pizza-react')
       .then((res) => res.json())
-      .then((arr) => 
-      setItems(arr));
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
   }, []);
     
       
@@ -29,10 +32,11 @@ function App() {
         </div>    
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
-          {
-            items.map(obj => 
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+            : items.map(obj => 
             <PizzaBlock
-            key={obj.title}
+            key={obj.id}
             {...obj} />)
           }
         </div>
